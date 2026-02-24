51660ba5cbfd6afe881dea8cebed2fe6ab8a0b75
aguirre-cuspian <laacblue1206@gmail.com>
2026-02-24 16:06:43 -0500

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  document.getElementById("footer").innerHTML = `
+  
+  <footer class="bg-primary text-white text-center py-4 mt-5">
+    <div class="container">
+      <h4 class="mb-4">Síguenos en redes sociales</h4>
+      <div class="mb-3">
+        
+        <a href="https//facebook.com" target="_blank" class="me-4">
+          <img src="images/face.png" width="30" alt="Facebook">
+        </a>
+
+        <a href="https://instagram.com" target="_blank" class="me-4">
+          <img src="images/instagran.png" width="50" alt="Instagram">
+        </a>
+
+        <a href="https://twitter.com" target="_blank" class="me-4">
+          <img src="images/twitter.png" width="50" alt="Twitter">
+        </a>
+
+      </div>
+      <p class="mb-0">© 2026 Sabores del Campo</p>
+    </div>
+  </footer>
+
+  `;
+});
